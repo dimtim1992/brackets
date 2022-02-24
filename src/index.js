99272d706125def796e71fe40d66913777b3c774
Dmitry Tsimoshchanka <36646929+dimtim1992@users.noreply.github.com>
2022-02-24 14:01:34 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let stack = [];
+  
+  if(str.length % 2 !== 0) {return false};
+  
+  for(item of str) {
+    
+    for(subArr of bracketsConfig) {
+      
+      if(item === subArr[0] && item === subArr[1]) {
+       stack[stack.length - 1] === item ? stack.pop() : stack.push(item); 
+      }
+      
+      if(item === subArr[0] && item !== subArr[1]) {
+        stack.push(item);
+      }
+      
+      if(item !== subArr[0] && item === subArr[1]) {
+        stack[stack.length - 1] === subArr[0] ? stack.pop() : false;
+      } 
+
+    }
+  }
+  return stack.length === 0;
 }
